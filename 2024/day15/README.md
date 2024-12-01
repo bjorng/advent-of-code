b565a753bb6a14a61165889096a24527ee68cda4
Björn Gustavsson <bgustavsson@gmail.com>
2024-12-01 05:03:04 +0100

    Dec 15, 2024

---
+++ 2024/day15/README.md
@@ -0,0 +1,5 @@
+# Day15
+
+My solution for Advent of Code 2024 - Day 15: https://adventofcode.com/2024/day/15
+
+Use `mix test` to run the code.
---
+++ 2024/day15/test/day15_test.exs
@@ -0,0 +1,81 @@
+defmodule Day15Test do
+  use ExUnit.Case
+  doctest Day15
+
+  test "part 1 with example" do
+    assert Day15.part1(small_example()) == 2028
+    assert Day15.part1(example()) == 10092
+  end
+
+  test "part 1 with my input data" do
+    assert Day15.part1(input()) == 1448589
+  end
+
+  test "part 2 with example" do
+    assert Day15.part2(small_example2()) == 618
+    assert Day15.part2(example()) == 9021
+  end
+
+  test "part 2 with my input data" do
+    assert Day15.part2(input()) == 1472235
+  end
+
+  defp small_example() do
+    """
+    ########
+    #..O.O.#
+    ##@.O..#
+    #...O..#
+    #.#.O..#
+    #...O..#
+    #......#
+    ########
+
+    <^^>>>vv<v>>v<<
+    """
+  end
+
+  defp example() do
+    """
+    ##########
+    #..O..O.O#
+    #......O.#
+    #.OO..O.O#
+    #..O@..O.#
+    #O#..O...#
+    #O..O..O.#
+    #.OO.O.OO#
+    #....O...#
+    ##########
+
+    <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+    vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+    ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+    <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+    ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+    ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+    >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+    <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+    ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+    v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+    """
+  end
+
+  defp small_example2() do
+    """
+    #######
+    #...#.#
+    #.....#
+    #..OO@#
+    #..O..#
+    #.....#
+    #######
+
+    <vv<<^^<<^^
+    """
+  end
+
+  defp input() do
+    File.read!("input.txt")
+  end
+end
