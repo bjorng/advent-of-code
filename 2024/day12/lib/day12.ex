0230fc95e4f65c242f21f696045d66a528a40733
Björn Gustavsson <bgustavsson@gmail.com>
2024-12-12 06:12:39 +0100

    wip

--- 2024/day12/lib/day12.ex
+++ 2024/day12/lib/day12.ex
@@ -7,10 +7,21 @@ defmodule Day12 do
     parse(input)
   end
 
+  defp adjacent_squares({row, col}) do
+    [{row - 1, col}, {row, col - 1}, {row, col + 1}, {row + 1, col}]
+  end
+
   defp parse(input) do
     input
-    |> Enum.map(fn line ->
-      line
+    |> Enum.with_index
+    |> Enum.flat_map(fn {line, row} ->
+      String.to_charlist(line)
+      |> Enum.with_index
+      |> Enum.flat_map(fn {char, col} ->
+        position = {row, col}
+        [{position, char - ?0}]
+      end)
     end)
+    |> Map.new
   end
 end
