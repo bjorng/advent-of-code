e700ff9e2ff7c6c44f140270ef63543e060ad37b
Björn Gustavsson <bgustavsson@gmail.com>
2025-12-10 06:32:35 +0100

    wip

--- 2025/day10/lib/day10.ex
+++ 2025/day10/lib/day10.ex
@@ -1,4 +1,6 @@
 defmodule Day10 do
+  import Bitwise
+
   def part1(input) do
     parse(input)
   end
@@ -9,5 +11,31 @@ defmodule Day10 do
 
   defp parse(input) do
     input
+    |> Enum.map(fn line ->
+      [lights | rest] = String.split(line, " ")
+
+      lights = String.slice(lights, 1, String.length(lights) - 2)
+      |> String.reverse
+      |> String.to_charlist
+      |> Enum.reduce(0, fn light, lights ->
+        lights = lights <<< 1
+        case light do
+          ?. -> lights
+          ?\# -> lights ||| 1
+        end
+      end)
+
+      {buttons, joltage} = Enum.split(rest, length(rest) - 1)
+
+      buttons = Enum.map(buttons, fn button ->
+        button
+        |> String.slice(1, String.length(button) - 2)
+        |> String.split(",")
+        |> Enum.map(&String.to_integer/1)
+        |> Enum.reduce(0, &((1 <<< &1) ||| &2))
+      end)
+
+      {lights, buttons, joltage}
+    end)
   end
 end
