9e47708b469cfd2d01cf7ad18aee9799be19eece
Björn Gustavsson <bgustavsson@gmail.com>
2025-12-10 06:01:08 +0100

    wip

--- 2025/day10/test/day10_test.exs
+++ 2025/day10/test/day10_test.exs
@@ -7,7 +7,7 @@ defmodule Day10Test do
   end
 
   test "part 1 with my input data" do
-    assert Day10.part1(input()) == nil
+#    assert Day10.part1(input()) == nil
   end
 
   test "part 2 with example" do
@@ -20,7 +20,9 @@ defmodule Day10Test do
 
   defp example() do
     """
-
+    [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+    [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+    [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
     """
     |> String.split("\n", trim: true)
   end
