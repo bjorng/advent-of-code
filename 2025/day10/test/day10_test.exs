a5a48969f16d385eb8dcf47e95f08d89c61f541e
Björn Gustavsson <bgustavsson@gmail.com>
2025-12-11 07:38:28 +0100

    Revert "wip"
    This reverts commit d779a2c8274be0b55d2166e9dc281c61a90fb453.

--- 2025/day10/test/day10_test.exs
+++ 2025/day10/test/day10_test.exs
@@ -11,15 +11,11 @@ defmodule Day10Test do
   end
 
   test "part 2 with example" do
-#    assert Day10.part2(example()) == 33
-  end
-
-  test "part 2 with example 2" do
-    assert Day10.part2(example2()) == 122
+    assert Day10.part2(example()) == 33
   end
 
   test "part 2 with my input data" do
-#    assert Day10.part2(input()) == nil
+    assert Day10.part2(input()) == nil
   end
 
   defp example() do
@@ -27,13 +23,6 @@ defmodule Day10Test do
     [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
     [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
     [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-    """
-    |> String.split("\n", trim: true)
-  end
-
-  defp example2() do
-    """
-    [..##...#.] (0,1,2,3,4) (2,3,4,5,6,8) (0,2,3,6,7) (1,2,3,4,5,7,8) (0,3,4,8) (1,2,3,4,5,6,8) (0,4,5,6,7,8) (0,1,3,6,8) (1,3,5,6,7) (0,3,5,7) {78,66,64,114,75,62,62,62,70}
     """
     |> String.split("\n", trim: true)
   end
