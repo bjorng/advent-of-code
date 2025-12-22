c7668b174f642ad1c61e58f289d81fb60b815802
Björn Gustavsson <bgustavsson@gmail.com>
2025-12-22 22:05:05 +0100

    Optimize solution for Dec 9
    All debug code stripped out and with additional optimizations.
    
    Finishes in 0.4 seconds on my computer.

--- 2025/day09/test/day09_test.exs
+++ 2025/day09/test/day09_test.exs
@@ -11,40 +11,18 @@ defmodule Day09Test do
   end
 
   test "part 2 with example" do
-#    assert Day09.part2(example(), [{2, 3}, {11, 7}]) == 0
-#    assert Day09.part2(example(), [{7, 3}, {11, 1}]) == 15
-#    assert Day09.part2(example(), [{9, 5}, {11, 7}]) == 9
-#    assert Day09.part2(example(), [{7, 1}, {9, 5}]) == 15
-#    assert Day09.part2(example(), [{7, 1}, {9, 7}]) == 0
-#    assert Day09.part2(example(), :all) == 24
     assert Day09.part2(example()) == 24
   end
 
   test "part 2 with example 2" do
-#    assert Day09.part2(example2(), [{3, 1}, {9, 11}]) == 0
-#    assert Day09.part2(example2(), [{1, 1}, {9, 11}]) == 0
-#    assert Day09.part2(example2(), [{7, 1}, {9, 11}]) == 33
-#    assert Day09.part2(example2(), [{3, 8}, {7, 1}]) == 0
-#    assert Day09.part2(example2(), [{3, 8}, {9, 1}]) == 0
-#    assert Day09.part2(example2(), :all) == 33
     assert Day09.part2(example2()) == 33
   end
 
   test "part 2 with example 3" do
-#    assert Day09.part2(example3(), [{17, 2}, {20, 11}]) == 40
-#    assert Day09.part2(example3(), [{7, 1}, {12, 5}]) == 30
-#    assert Day09.part2(example3(), [{7, 1}, {14, 9}]) == 0
-#    assert Day09.part2(example3(), [{1, 11}, {17, 9}]) == 51
-#    assert Day09.part2(example3(), :all) == 51
     assert Day09.part2(example3()) == 51
   end
 
   test "part 2 with my input data" do
-    # 4752484112 is too high
-    # 4704016240 is also too high
-    # 3043138441 is also too high
-#    assert Day09.part2(input(), [{15179, 15977}, {84863, 83480}]) == 0
-#    assert Day09.part2(input(), [{1890, 50228}, {73212, 92894}]) == 0
     assert Day09.part2(input()) == 1465767840
   end
 
